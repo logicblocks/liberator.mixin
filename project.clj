72a43bdae9d9e4a6af2d82953ef0e52ad361fc8f
jimmythompson <jimmy@jimmythompson.co.uk>
2019-02-03 23:31:34 +0000

    [TECH] Bump version to 0.0.2
    Authors: @jimmythompson

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.b-social/microservice-tools "0.0.1"
+(defproject com.b-social/microservice-tools "0.0.2"
   :description "A collection of sensible defaults for liberator microservices"
   :url "https://github.com/b-social/microservice-tools"
   :license "Proprietary"
