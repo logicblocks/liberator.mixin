2488a58347a7258633ba7eb3702c77d9f76298a7
Toby Clemson <tobyclemson@gmail.com>
2019-09-12 15:18:34 +0100

    Version 0.0.26-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject b-social/liberator-mixin "0.0.25"
+(defproject b-social/liberator-mixin "0.0.26-SNAPSHOT"
   :description "An extension to liberator allowing for composable mixins."
   :url "https://github.com/b-social/liberator-mixin"
 
