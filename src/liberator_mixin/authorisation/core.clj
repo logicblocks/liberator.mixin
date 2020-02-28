8a376b1203255bebb7e29a0b2b6183df7776591c
Alex Parlett <alexparlett@gmail.com>
2020-02-28 21:49:23 +0000

    Improvements for liberator style config
    - Updated doc strings
    - Added token pre parser
    - Added scope validator

--- src/liberator_mixin/authorisation/core.clj
+++ src/liberator_mixin/authorisation/core.clj
@@ -1,10 +1,9 @@
 (ns liberator-mixin.authorisation.core
   "Liberator mixin to authorise a request based on an access token"
-  (:require [clojure.string :as str]
-            [buddy.auth.http :as http]
-            [buddy.sign.jwt :as jwt]
-            [liberator-mixin.json.core :refer [with-json-media-type]]
-            [clojure.string :as string])
+  (:require [buddy.auth.http :as http]
+    [buddy.sign.jwt :as jwt]
+    [liberator-mixin.json.core :refer [with-json-media-type]]
+    [clojure.string :as string])
   (:import [clojure.lang ExceptionInfo]))
 
 (defn- parse-header
@@ -22,11 +21,11 @@
 
 (defn- to-error
   ([message]
-   (to-error message {}))
+    (to-error message {}))
   ([message data]
-   {:authorised? false
-    :error       {:message message
-                  :data    data}}))
+    {:authorised? false
+     :error       {:message message
+                   :data    data}}))
 
 (defn- is-authorised? [token-claims claims]
   (if (empty? token-claims)
@@ -36,12 +35,16 @@
                 :let [key (first token-claim)
                       validator (second token-claim)
                       found (key claims)]]
-            {:authorised? (and (some? found) (validator found))
-             :error       {:message (format "Access token failed validation for %s." (name key))
-                           :data    {:type :validation :cause key}}})]
+            {:authorised?
+             (and (some? found) (true? (validator found)))
+             :error
+             {:message (format "Access token failed validation for %s."
+                         (name key))
+              :data    {:type :validation :cause key}}})]
       (reduce (fn [state curr]
-                {:authorised? (and (:authorised? state) (:authorised? curr))
-                 :error       (merge (:error state) (:error curr))})
+                (let [authorised? (:authorised? curr)]
+                  {:authorised? (and (:authorised? state) authorised?)
+                   :error       (if authorised? (:error state) (:error curr))}))
         results))))
 
 (defn- parse-token [token-claims token-key token-opts data]
@@ -60,32 +63,35 @@
     {:type :validation :cause :token}))
 
 (defn with-jws-access-token
-  "Returns a mixin that validates the jws access token ensure it includes the claims
-  and that claim passes validation, finally it stores the authentication and authorisation
-  state on the context under :identity
+  "Returns a mixin that validates the jws access token ensure it includes the
+  claims and that claim passes validation, finally it stores the authentication
+  and authorisation state on the context under :identity
 
-  The secret can be a function which is provided the JOSE header as its single
-  param
-
-  Takes token as an optional param that changes the type of token looked for
-  (default is Bearer)
-
-  Takes opts as an optional param that is used to validate the claims of the
-  token (aud, iss, sub, exp, nbf, iat)
+  :token-type - the scheme under the authorisation header (default is Bearer)
+  :token-key - the secret can be a function which is provided the JOSE header
+  as its single param
+  :token-options - that is used to validate the standard claims of the
+  token (aud, iss, sub, exp, nbf, iat) (optional)
+  :token-claims - a map of expected claims and how to validate them as function
+  that takes the claim value (optional)
+  :token-parser - a function that performs parsing of the token before
+  validation (optional)
 
   This mixin should only be used once."
   []
-  {:initialize-context (fn [{:keys [request resource]}]
-                         (let [token-type (get resource :token-type (constantly "Bearer"))
-                               token-options (get resource :token-options (constantly {}))
-                               token-key (get resource :token-key)
-                               token-claims (get resource :token-claims (constantly {}))
-                               token-parser (get resource :token-parser identity)
-                               token (parse-header request (token-type) token-parser)]
-                           (if (some? token)
-                             (parse-token (token-claims) (token-key) (token-options) token)
-                             (missing-token (token-type)))))
-   :authorized?        (fn [{:keys [authorised?]}] authorised?)})
+  {:initialize-context
+   (fn [{:keys [request resource]}]
+     (let [token-type (get resource :token-type (constantly "Bearer"))
+           token-options (get resource :token-options (constantly {}))
+           token-key (get resource :token-key)
+           token-claims (get resource :token-claims (constantly {}))
+           token-parser (get resource :token-parser identity)
+           token (parse-header request (token-type) token-parser)]
+       (if (some? token)
+         (parse-token (token-claims) (token-key) (token-options) token)
+         (missing-token (token-type)))))
+   :authorized?
+   (fn [{:keys [authorised?]}] authorised?)})
 
 (defn- error-to-status
   [{:keys [data]}]
@@ -111,7 +117,7 @@
     "error_message=\"" message "\"\n"))
 
 (defn with-www-authenticate
-  "Returns a mixin populates the WWW-Authenticate error when the
+  "Returns a mixin that populates the WWW-Authenticate error when the
   JWT is not authorised to access the protected endpoint.
 
   This mixin should only be used once."
@@ -121,3 +127,11 @@
      (liberator.representation/ring-response
        {:status  (error-to-status error)
         :headers {"WWW-Authenticate" (error-to-header error)}}))})
+
+(defn scope-validator
+  "Returns a validator that ensures all required scopes are included in the
+  token."
+  [required-scopes]
+  (fn [scope]
+    (every? #(contains? (set required-scopes) %)
+      (string/split scope #" "))))
--- test/liberator_mixin/authorisation/core_test.clj
+++ test/liberator_mixin/authorisation/core_test.clj
@@ -8,10 +8,12 @@
     [ring.mock.request :as ring]
     [buddy.sign.jwt :refer [sign]]
     [liberator-mixin.core :as core]
-    [liberator-mixin.authorisation.core :as jws]
+    [liberator-mixin.authorisation.core :as auth]
     [liberator-mixin.json.core :as json]
     [jason.convenience :refer [<-wire-json]]
-    [clojure.string :as string]))
+    [clojure.string :as string]
+    [buddy.core.codecs.base64 :as b64]
+    [buddy.core.codecs :as codecs]))
 
 (defn call-resource [resource request]
   (->
@@ -22,14 +24,13 @@
   (testing "the resource is authorised with the right scopes available"
     (let [resource (core/build-resource
                      (json/with-json-media-type)
-                     (jws/with-jws-access-token)
-                     (jws/with-www-authenticate)
-                     {:token-claims {:scope (fn [scope]
-                                              (every? #(contains? #{"read"} %) (string/split scope #" ")))}
+                     (auth/with-jws-access-token)
+                     (auth/with-www-authenticate)
+                     {:token-claims {:scope (auth/scope-validator #{"read"})}
                       :token-key    "foo"
                       :handle-ok
-                                    (fn [{:keys [routes]}]
-                                      routes)})
+                      (fn [{:keys [routes]}]
+                        routes)})
           request (ring/request :get "/")
           request (ring/header
                     request
@@ -40,13 +41,66 @@
                      request)]
       (is (= 200 (:status response)))))
 
+  (testing "the resource is not authorised with multiple claims"
+    (let [resource (core/build-resource
+                     (json/with-json-media-type)
+                     (auth/with-jws-access-token)
+                     (auth/with-www-authenticate)
+                     {:token-claims {:scope (auth/scope-validator #{"read"})
+                                     :failed (constantly false)}
+                      :token-key    "foo"
+                      :handle-ok
+                      (fn [{:keys [routes]}]
+                        routes)})
+          request (ring/request :get "/")
+          request (ring/header
+                    request
+                    "authorization"
+                    (str "Bearer " (sign {:scope "read"} "foo")))
+          response (call-resource
+                     resource
+                     request)
+          header (get-in response [:headers "WWW-Authenticate"])]
+      (is (= 401 (:status response)))
+      (is (string/includes?
+            header
+            "Access token failed validation for failed."))))
+
+  (testing "the resource is not authorised with multiple claims contains last"
+    (let [resource (core/build-resource
+                     (json/with-json-media-type)
+                     (auth/with-jws-access-token)
+                     (auth/with-www-authenticate)
+                     {:token-claims {:scope (auth/scope-validator #{"read"})
+                                     :failed (constantly false)}
+                      :token-key    "foo"
+                      :handle-ok
+                      (fn [{:keys [routes]}]
+                        routes)})
+          request (ring/request :get "/")
+          request (ring/header
+                    request
+                    "authorization"
+                    (str "Bearer " (sign {:scope "write"} "foo")))
+          response (call-resource
+                     resource
+                     request)
+          header (get-in response [:headers "WWW-Authenticate"])]
+      (is (= 401 (:status response)))
+      (is (string/includes?
+            header
+            "Access token failed validation for failed."))))
+
   (testing "the resource doesnt care about the case of bearer"
     (let [resource (core/build-resource
                      (json/with-json-media-type)
-                     (jws/with-jws-access-token)
-                     (jws/with-www-authenticate)
-                     {:token-claims {:scope (fn [scope]
-                                              (every? #(contains? #{"read"} %) (string/split scope #" ")))}
+                     (auth/with-jws-access-token)
+                     (auth/with-www-authenticate)
+                     {:token-claims {:scope
+                                     (fn [scope]
+                                       (every?
+                                         #(contains? #{"read"} %)
+                                         (string/split scope #" ")))}
                       :token-key    "foo"
                       :handle-ok
                       (fn [{:keys [routes]}]
@@ -61,15 +115,67 @@
                      request)]
       (is (= 200 (:status response)))))
 
+  (testing "the resource is authorised with a different token scheme"
+    (let [resource (core/build-resource
+                     (json/with-json-media-type)
+                     (auth/with-jws-access-token)
+                     (auth/with-www-authenticate)
+                     {:token-claims {:scope (fn [scope]
+                                              (every?
+                                                #(contains? #{"read"} %)
+                                                (string/split scope #" ")))}
+                      :token-type   "Token"
+                      :token-key    "foo"
+                      :handle-ok
+                      (fn [{:keys [routes]}]
+                        routes)})
+          request (ring/request :get "/")
+          request (ring/header
+                    request
+                    "authorization"
+                    (str "Token " (sign {:scope "read"} "foo")))
+          response (call-resource
+                     resource
+                     request)]
+      (is (= 200 (:status response)))))
+
+  (testing "the resource is authorised when encoded"
+    (let [resource (core/build-resource
+                     (json/with-json-media-type)
+                     (auth/with-jws-access-token)
+                     (auth/with-www-authenticate)
+                     {:token-claims {:scope (fn [scope]
+                                              (every?
+                                                #(contains? #{"read"} %)
+                                                (string/split scope #" ")))}
+                      :token-parser (fn [token]
+                                      (-> (b64/decode token)
+                                        (buddy.core.codecs/bytes->str)))
+                      :token-key    "foo"
+                      :handle-ok
+                      (fn [{:keys [routes]}]
+                        routes)})
+          request (ring/request :get "/")
+          request (ring/header
+                    request
+                    "authorization"
+                    (str "Bearer " (codecs/bytes->str
+                                     (b64/encode
+                                       (sign {:scope "read"} "foo") true))))
+          response (call-resource
+                     resource
+                     request)]
+      (is (= 200 (:status response)))))
+
   (testing "the resource is authorised when no scopes required"
     (let [resource (core/build-resource
                      (json/with-json-media-type)
-                     (jws/with-jws-access-token)
-                     (jws/with-www-authenticate)
+                     (auth/with-jws-access-token)
+                     (auth/with-www-authenticate)
                      {:token-key "foo"
                       :handle-ok
-                                 (fn [{:keys [routes]}]
-                                   routes)})
+                      (fn [{:keys [routes]}]
+                        routes)})
           request (ring/request :get "/")
           request (ring/header
                     request
@@ -83,15 +189,15 @@
   (testing "the resource is not authorised with the right scopes available"
     (let [resource (core/build-resource
                      (json/with-json-media-type)
-                     (jws/with-jws-access-token)
-                     (jws/with-www-authenticate)
+                     (auth/with-jws-access-token)
+                     (auth/with-www-authenticate)
                      {:token-claims {:scope (fn [scope]
                                               (every? #(contains? #{"read"} %)
-                                                      (string/split scope #" ")))}
+                                                (string/split scope #" ")))}
                       :token-key    "foo"
                       :handle-ok
-                                    (fn [{:keys [routes]}]
-                                      routes)})
+                      (fn [{:keys [routes]}]
+                        routes)})
           request (ring/request :get "/")
           request (ring/header
                     request
@@ -109,12 +215,12 @@
   (testing "the resource does not have scope claim"
     (let [resource (core/build-resource
                      (json/with-json-media-type)
-                     (jws/with-jws-access-token)
-                     (jws/with-www-authenticate)
+                     (auth/with-jws-access-token)
+                     (auth/with-www-authenticate)
                      {:token-claims {:scope (fn [scope]
                                               (every? #(contains? #{"read"} %)
-                                                      (string/split scope #" ")))}
-                      :token-key "foo"
+                                                (string/split scope #" ")))}
+                      :token-key    "foo"
                       :handle-ok
                       (fn [{:keys [routes]}]
                         routes)})
@@ -128,13 +234,14 @@
                      request)
           header (get-in response [:headers "WWW-Authenticate"])]
       (is (= 403 (:status response)))
-      (is (string/includes? header "Access token failed validation for scope."))))
+      (is (string/includes? header
+            "Access token failed validation for scope."))))
 
   (testing "the token is under the wrong identifier"
     (let [resource (core/build-resource
                      (json/with-json-media-type)
-                     (jws/with-jws-access-token)
-                     (jws/with-www-authenticate)
+                     (auth/with-jws-access-token)
+                     (auth/with-www-authenticate)
                      {:token-key "foo"
                       :handle-ok
                       (fn [{:keys [routes]}]
@@ -153,11 +260,34 @@
             header
             "Message does not contain a Bearer token."))))
 
+  (testing "the token is not signed with the same key"
+    (let [resource (core/build-resource
+                     (json/with-json-media-type)
+                     (auth/with-jws-access-token)
+                     (auth/with-www-authenticate)
+                     {:token-key "foo"
+                      :handle-ok
+                      (fn [{:keys [routes]}]
+                        routes)})
+          request (ring/request :get "/")
+          request (ring/header
+                    request
+                    "authorization"
+                    (str "Bearer " (sign {:scope "read"} "bar")))
+          response (call-resource
+                     resource
+                     request)
+          header (get-in response [:headers "WWW-Authenticate"])]
+      (is (= 401 (:status response)))
+      (is (string/includes?
+            header
+            "Message seems corrupt or manipulated."))))
+
   (testing "the token has expired"
     (let [resource (core/build-resource
                      (json/with-json-media-type)
-                     (jws/with-jws-access-token)
-                     (jws/with-www-authenticate)
+                     (auth/with-jws-access-token)
+                     (auth/with-www-authenticate)
                      {:token-key "foo"
                       :handle-ok
                       (fn [{:keys [routes]}]
@@ -169,7 +299,7 @@
                     "authorization"
                     (str "Bearer " (sign {:scope "read"
                                           :exp   expiry-time}
-                                         "foo")))
+                                     "foo")))
           response (call-resource
                      resource
                      request)]
@@ -178,9 +308,9 @@
   (testing "does not meet the required audience"
     (let [resource (core/build-resource
                      (json/with-json-media-type)
-                     (jws/with-jws-access-token)
-                     (jws/with-www-authenticate)
-                     {:token-key "foo"
+                     (auth/with-jws-access-token)
+                     (auth/with-www-authenticate)
+                     {:token-key     "foo"
                       :token-options {:aud "pms.com"}
                       :handle-ok
                       (fn [{:keys [routes]}]
@@ -191,7 +321,7 @@
                     "authorization"
                     (str "Bearer " (sign {:scope "read"
                                           :aud   "developers.com"}
-                                         "foo")))
+                                     "foo")))
           response (call-resource
                      resource
                      request)
