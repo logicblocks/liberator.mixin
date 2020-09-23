c85cb80d75e96a165aaafa2a32cb430b270f59dc
Alex Parlett <alexparlett@gmail.com>
2020-09-23 16:10:36 +0100

    Version 0.0.57-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject b-social/liberator-mixin "0.0.56"
+(defproject b-social/liberator-mixin "0.0.57-SNAPSHOT"
   :description "An extension to liberator allowing for composable mixins."
   :url "https://github.com/b-social/liberator-mixin"
 
