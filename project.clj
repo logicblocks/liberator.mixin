8006649cc922ccd9df4e28d82a9585e1aaaec590
Paul Hutchin <paul.hutchin@kroo.com>
2022-06-28 17:05:15 +0100

    Revert "[TECH] Release 0.0.58"
    This reverts commit 96ac15da428f57b8cbb42797b2b55d4b21e96656.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject b-social/liberator-mixin "0.0.58"
+(defproject b-social/liberator-mixin "0.0.58-SNAPSHOT"
   :description "An extension to liberator allowing for composable mixins."
   :url "https://github.com/b-social/liberator-mixin"
 
